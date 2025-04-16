fea026adf71283ea7326cd4a4b1cfc9a1b362ddc
spycode92 <spycode92@gmail.com>
2025-04-16 17:45:14 +0900

    Add files via upload

---
+++ Backend/workspace_backend/StudyJavascript/src/main/webapp/javascript5_0416_event/test1.js
@@ -0,0 +1,47 @@
+function btnClick() {
+	alert("버튼4 클릭");
+}
+
+function btnClick2(msg) {
+	alert(msg);
+}
+
+function btnClick3(num1, num2) {
+	alert(num1 + num2);
+}
+// --------------------------------------------
+function startNumberGame() {
+	let correctNum = 5; // 정답
+	
+	
+	if(숫자 입력 여부 판별) {
+		return; // 현재 함수(startNumberGame) 를 종료하고 빠져나감
+	}
+	
+	
+	// 3번 풀 때 필요한 코드(무한루프 사용 및 무한루프 빠져나오기) 
+	while(true) {
+		// 정답일 경우 판별
+		if() {
+			break; // 현재 while 문을 종료하고 빠져나감(Exit while)
+		}	
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
